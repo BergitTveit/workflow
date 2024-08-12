26659b5ab7e6c53af1b795dce9be71ca99d8c377
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-08-12 13:53:31 +0200

    Update ESLint and Prettier configurations, and package files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import { defineFlatConfig } from 'eslint-define-config';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+const recommendedJsRules = pluginJs.configs.recommended.rules;
+
+const prettierConfigRules = configPrettier.rules || {};
+
+export default defineFlatConfig([
+    {
+        languageOptions: {
+            globals: globals.browser,
+        },
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+      
+            ...recommendedJsRules,
 
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+            'prettier/prettier': 'error',
+
+            ...prettierConfigRules,
+        },
+    },
+]);
